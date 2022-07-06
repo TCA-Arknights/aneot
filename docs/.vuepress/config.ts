2a066c89e191225f95375952c576c8608a66e555
Nemo Zhang <zhangguoxi1166@gmail.com>
2022-07-06 19:15:02 +0800

    add baidu analytics

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -15,6 +15,19 @@ export default defineHopeConfig({
         href: "//at.alicdn.com/t/font_2410206_mfj6e1vbwo.css",
       },
     ],
+    [
+      "script",
+      {},
+      `
+      var _hmt = _hmt || [];
+      (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?5a7388e8b9935ce907b84d7147b3a31d";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
+      })();
+      `
+    ],
   ],
   themeConfig,
 });
