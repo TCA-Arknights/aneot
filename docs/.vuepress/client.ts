013d6feb732d761367fcb68e90f3cb23e0213089
Nemo Zhang <zhangguoxi1166@gmail.com>
2022-10-16 22:38:16 +0800

    misc change

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -1,10 +1,6 @@
 import { defineClientConfig } from "@vuepress/client";
-import AdsenseInline from "./components/ArticleAd.vue";
-import eod from "./components/eod.vue";
 
 export default defineClientConfig({
     enhance: ({ app, router, siteData }) => {
-        app.component("ArticleAd", AdsenseInline)
-        .component("eod", eod);
     },
 });
