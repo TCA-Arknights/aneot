7c14c51e2bb5faaa49462b4cee6773c9a5726b4c
Mr. Lei <YigeYigeren@outlook.com>
2023-02-17 12:20:26 -0500

    Update

---
+++ docs/.vuepress/components/FakeAd.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="FakeAd"></div>
+</template>
+
+// 该片段用于泰拉广告随机播放，尚未完工，这里是草稿
+<template>
+  <div class="ads-container">
+    <p class="ads-hint">此处可能展示泰拉广告。<a href="/about-fake-ads">了解详情</a></p>
+  </div>
+</template>
+
+<script language=javascript>
+id=Math.round(Math.random()*2)+1
+document.write(".\docs\.vuepress\fake-ads\<img src="+id+".webp>")
+document.write("<a href="https://www.bilibili.com/video/BV1GJ411x7h7/">"+".\docs\.vuepress\fake-ads\<img src="+id+".webp>")
+</script>
