0036e6b928efe0f0fd7476b6b4eb81136c150805
Mr. Lei <YigeYigeren@outlook.com>
2023-03-31 15:48:57 -0400

    更新 enhanceApp.js

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,4 +1,5 @@
-export default ({ router, Vue }) => {
+// 在VuePress加载完成后执行
+export default ({ Vue }) => {
     // 添加一个全局的混入
     Vue.mixin({
       mounted() {
@@ -27,19 +28,6 @@ export default ({ router, Vue }) => {
         // 将div元素添加到文档中
         document.body.appendChild(watermark);
       }
-    });
-  
-    // 路由切换事件处理
-    router.beforeEach((to, from, next) => {
-      // console.log("切换路由", to.fullPath, from.fullPath);
-      //触发百度的pv统计
-      if (typeof _hmt != "undefined") {
-        if (to.path) {
-          _hmt.push(["_trackPageview", to.fullPath]);
-          // console.log("上报百度统计", to.fullPath);
-        }
-      }
-      next();
     });
   };
   
