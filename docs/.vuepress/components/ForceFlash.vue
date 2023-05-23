6945660a8666a6feb7fcabce53b0804e2aac25f3
Mr. Lei <YigeYigeren@outlook.com>
2023-05-23 14:33:23 -0400

    Style Fix & New Feature (#64)

---
+++ docs/.vuepress/components/ForceFlash.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>正在载入更新，请稍等</div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ForceFlash',
+    mounted() {
+      if (localStorage.getItem('forceflashReloaded')) {
+        localStorage.removeItem('forceflashReloaded');
+        this.$router.push('/');
+      } else {
+        localStorage.setItem('forceflashReloaded', 'true');
+        location.reload();
+      }
+    },
+  }
+  </script>
+  
