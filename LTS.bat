b87320edf4b47a36003919887952768d170ae254
Mr. Lei <YigeYigeren@outlook.com>
2023-07-10 15:00:42 -0400

    Update LTS.bat

--- LTS.bat
+++ LTS.bat
@@ -1,16 +1,29 @@
 @title VuePress Local Testing Services
-@rem This script is suitable for rapid deployment, support to add it to the debugging of your development environment.
-@If not exist node_modules goto ins
+@rem This script is suitable for rapid deployment, support to add it to the debugging of your development environment(add parameter cmd).
 
-@echo The browser will be automatically pulled up when the preview file is ready, if it is not pulled up successfully, please type in the browser address bar ^"http://localhost:8080^"
-@echo Please wait for the command line to appear ^"vite vX.X.X devserver running at:^" Just wait a few seconds to refresh the page after it appears
+@If "%~1"=="cmd" (
+    @rem Running from command line
+    @GOTO ser
+) ELSE (
+    @rem Running by double clicking, start a new cmd.exe and rerun this script from it
+    start cmd /k "cd /d "%~dp0" && "%~0" cmd"
+    exit /b
+)
+
+@If not exist node_modules goto ins
+@echo The browser will be pulled up, if it is not pulled up successfully, please type in the browser address bar ^"http://localhost:8080^"
+@echo Just wait a few seconds to refresh the page after it appears
 pnpm run docs:dev --open
 exit
 
 @:ins
-@echo No VuePress dependency detected, trying to install, please start this script again after installation is complete
+@echo No VuePress dependency detected, trying to install... Please run again before successfully install
 @timeout /t 3 >nul
 pnpm install
-If not %errorlevel%==0 echo This service relies on the pnpm command, please make sure it is installed globally
-@timeout /t 10 >nul
-exit
+@If not %errorlevel%==0 (
+    @echo This service relies on the pnpm command, please make sure it is installed globally
+    @timeout /t 10 >nul
+    exit /b
+)
+
+
