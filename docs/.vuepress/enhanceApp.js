f9b5cdca41445c16df05952e6cdde3507efe103f
Mr. Lei <YigeYigeren@outlook.com>
2023-07-16 11:33:34 -0400

    Revert "Revert "update fix""
    This reverts commit 7b3963d7a0344af98918376d316a73d2644f1a8d.

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,6 +1,51 @@
 export default ({ router }) => {
     // 路由切换事件处理
     router.beforeEach((to, from, next) => {
+    // 检查浏览器是否过旧
+    function isOldBrowser() {
+      const userAgent = window.navigator.userAgent;
+
+      // 检查是否是 Internet Explorer
+      if (userAgent.includes('MSIE') || userAgent.includes('Trident')) {
+        return true;
+      }
+
+      // 检查 Chrome/Chromium 版本
+      const chromeMatch = userAgent.match(/Chrome\/(\d+)/);
+      if (chromeMatch && Number(chromeMatch[1]) < 64) {
+        return true;
+      }
+
+      // 检查 Firefox 版本
+      const firefoxMatch = userAgent.match(/Firefox\/(\d+)/);
+      if (firefoxMatch && Number(firefoxMatch[1]) < 58) {
+        return true;
+      }
+
+      // 检查 Safari 版本
+      const safariMatch = userAgent.match(/Version\/(\d+)/);
+      if (safariMatch && Number(safariMatch[1]) < 11) {
+        return true;
+      }
+
+      // 检查 Edge 版本
+      const edgeMatch = userAgent.match(/Edge\/(\d+)/);
+      if (edgeMatch && Number(edgeMatch[1]) < 16) {
+        return true;
+      }
+
+      return false;
+    }
+
+    router.beforeEach((to, from, next) => {
+      if (isOldBrowser()) {
+        if (confirm('你的浏览器版本可能过旧。是否要跳转到我们的兼容版网站以获取最佳体验？')) {
+          window.location.href = 'https://old.aneot.terrach.net' + to.fullPath;
+          return;
+        }
+      }
+
+        
         // console.log("切换路由", to.fullPath, from.fullPath);
         //触发百度的pv统计
         if (typeof _hmt != "undefined") {
@@ -11,5 +56,12 @@ export default ({ router }) => {
         }
         next();
     });
-};
 
+    /*
+    router.afterEach((to, from) => {
+      if (/micromessenger|qq\//i.test(navigator.userAgent)) {
+        router.push({ path: '/BrowserCheck', query: { redirect: to.fullPath } });
+      }
+    */
+  });
+};
--- docs/README.md
+++ docs/README.md
@@ -6,9 +6,9 @@ title: 主页
 heroText: 回归线
 heroFullScreen: false
 # 在这里修改抬头画师
-tagline: © 西月将倾 2023 · 06
+tagline: © 迷迭香
 # 在这里修改版头主题色
-heroAlt: '#bd3b2e'
+heroAlt: '#79a5c8'
 projects:
   - icon: read
     name: 卷首
@@ -22,8 +22,8 @@ projects:
 
   - icon: article
     name: 最新一期
-    desc: Vol.13 2023-06
-    link: posts/2023-06
+    desc: Vol.14 2023-07
+    link: posts/2023-07
 
   - icon: cache
     name: 下载 PDF
