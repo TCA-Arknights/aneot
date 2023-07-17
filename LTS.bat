9f219e766934c124763e65113528f52593331156
Mr. Lei <YigeYigeren@outlook.com>
2023-07-17 14:51:50 -0400

    new feature

--- LTS.bat
+++ LTS.bat
@@ -12,48 +12,84 @@
 @rem github.com/yige-yigeren                 
 
 @rem if you use in the cammand line, please add parameter cmd, or it will open in default cammand line
-@rem Example: LTS.cmd cmd
+@rem default is dev build
+@rem Example: LTS.bat cmd
 @rem enable parameter clean, clean up the cache and node_modules
-@rem Example: LTS.cmd clean
+@rem Example: LTS.bat clean
 @rem enable parameter reset, clean up the cache and node_modules, and then install dependencies
-@rem Example: LTS.cmd reset
-@rem If you want don't want to open new cammand line, please add parameter cmd
-@rem Example: LTS.cmd cmd clean/reset
+@rem Example: LTS.bat reset
+
 @IF "%~1" NEQ "cmd" (
     @rem Running by double clicking, start a new cmd.exe and rerun this script from it
     start cmd /k "cd /d "%~dp0" && "%~0" cmd "%~1""
     exit /b
-) ELSE If "%~2"=="clean" (
-    @rem Clean up the cache and node_modules
-    @GOTO cle
-) ELSE If "%~2"=="reset" (
-    @rem Clean up the cache and node_modules, and then install dependencies
-    @set int=true
-    @GOTO cle
 ) ELSE (
-    @rem Running from command line
-    @GOTO ser
+    If "%~2"=="delate" (
+        @rem del all cache and dependencies
+        @GOTO del
+    ) ELSE If "%~2"=="reset" (
+        @rem Clean up the cache and node_modules, and then install dependencies
+        @set int=true
+        @GOTO del
+    ) ELSE IF NOT exist node_modules (
+        @rem no exist dependencies, install 
+        @IF NOT "%~2"=="" (
+            @goto ins 
+        )
+        @echo Cannot execute the command because the required dependency does not exist.
+        choice /T 5 /C yn /D y /M "Do you want to install it now?"
+        IF ERRORLEVEL 2 (
+            @echo Please install the dependency manually and run again
+            @timeout /t 10 >nul
+            exit /b
+        ) ELSE (
+            @GOTO ins
+        )
+    ) ELSE If "%~2"=="clean" (
+        @rem Clean up the cache
+        @GOTO cle
+    ) ELSE If "%~2"=="build" (
+        @rem Build the project
+        @GOTO bui
+    ) ELSE If "%~2"=="update" (
+        @rem Build the project
+        @GOTO upd
+    ) ELSE (
+        @rem Running from command line
+        @GOTO ser
+    )
 )
 
 @:ser
-@If not exist node_modules goto ins
 @echo The browser will be pulled up, if it is not pulled up successfully, please type in the browser address bar ^"http://localhost:****^"
 @echo Just wait a few seconds to refresh the page after it appears
 pnpm run docs:dev --open
 exit /b
 
 @:cle
+pnpm run docs:clean-dev
+exit /b
+
+@:del
 @rem delete node_modules \docs\.vuepress\dist \docs\.vuepress\.temp \docs\.vuepress\.cache 
 @echo Clean up the cache and node_modules
 @echo Please wait...
 @timeout /t 3 >nul
 rd /s /q node_modules
 rd /s /q docs\.vuepress\dist
-rd /s /q docs\.vuepress\.temp
+d /s /q docs\.vuepress\.temp
 rd /s /q docs\.vuepress\.cache
 @If "%int%"=="true" (
     @GOTO ins
 )
+exit /b
+
+@:bui
+pnpm run docs:build
+exit /b
+
+@:upd
+pnpm dlx vp-update
 exit /b
 
 @:ins
