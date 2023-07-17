e21ed9736d68a3fe5d05355b2ac6e2a4607780bb
Mr. Lei <YigeYigeren@outlook.com>
2023-07-17 14:55:52 -0400

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -13,6 +13,15 @@ export default defineUserConfig({
   base: "/",
   shouldPrefetch: false,
   head: [
+      ['link', { rel: 'icon', href: '/logo.png' }],
+      ['link', { rel: 'manifest', href: '/manifest.json' }],
+      ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+      ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+      ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+      ['link', { rel: 'apple-touch-icon', href: '/logo.png' }],
+      ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+      ['meta', { name: 'msapplication-TileImage', content: '/logo.png' }],
+      ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
     [
       "link",
       {
