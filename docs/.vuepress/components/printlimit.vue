8bc6fa984d53b3fdf8eb5dfc04e4105d8809630f
Mr. Lei <YigeYigeren@outlook.com>
2023-08-18 10:18:46 -0400

    preload

--- docs/.vuepress/components/printlimit.vue
+++ docs/.vuepress/components/printlimit.vue
@@ -14,7 +14,7 @@
   
          if (result) {
          // 点击确定时打开关于页面
-         window.open('/limit#关于网页', '_blank');
+         window.open('/bulletin/access_limit#关于网页', '_blank');
         }
 
       };
