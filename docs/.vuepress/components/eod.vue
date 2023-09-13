2c8b741fc0eb33661ecf932ce501975b7986e1dc
Mr. Lei <YigeYigeren@outlook.com>
2023-09-12 21:00:30 -0400

    Update eod.vue

--- docs/.vuepress/components/eod.vue
+++ docs/.vuepress/components/eod.vue
@@ -1,5 +1,5 @@
 <template>
-<span><img src="/eod.png" /></span>
+  <span><img class="eod-image" src="/eod.png" /></span>
 </template>
 
 <script>
@@ -9,4 +9,14 @@ export default {
 </script>
 
 <style>
+.eod-image {
+  /* 正常模式下的样式 */
+}
+
+/* 暗色模式下自动反色 */
+@media (prefers-color-scheme: dark) {
+  .eod-image {
+    filter: invert(100%);
+  }
+}
 </style>
