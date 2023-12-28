6cede14a0c48a8f9240fa70cdb5342a76432f599
Mr. Lei <YigeYigeren@outlook.com>
2023-12-27 21:35:57 -0500

    fix

---
+++ docs/.vuepress/components/refresh.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'refresh',
+
+  mounted() {
+    // 检查是否需要刷新
+    if (!window.location.hash.includes('refreshed')) {
+      window.location.href = window.location.href.split('#')[0] + '#refreshed';
+      window.location.reload();
+    }
+  }
+};
+</script>
