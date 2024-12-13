a1a2249f38c648aef15b9f6f8130837a1a2de230
Baka632 <stevemc123456@outlook.com>
2024-12-13 14:05:02 +0800

    - 更新 Hero 图及主题色 - 更新封面图画师信息 - 将 <eod/> 组件设为不可选中，可使 <eod/> 组件居右的 eod-aright 样式 - 更改《致特蕾西娅》的引文，调整文末 <eod/> 的位置 - 将《特蕾西娅小姐》的标题改为《特蕾西娅小姐？》 - 将《Seasons will pass you by》中“And You And I”一节改为斜体，并在文末添加落款及时间 - 调整本期画中秘境中插画的顺序

--- docs/.vuepress/components/eod.vue
+++ docs/.vuepress/components/eod.vue
@@ -1,12 +1,11 @@
 <template>
-  <span><img src="/eod_auto.svg" width="13" height="13"/></span>
-  </template>
-  
-  <script>
-  export default {
-    name: "eod",
-  }
-  </script>
-  
-  <style>
-  </style>
+  <span class="no-interact"><img alt="" src="/eod_auto.svg" width="13" height="13" /></span>
+</template>
+
+<script>
+export default {
+  name: "eod"
+}
+</script>
+
+<style></style>
